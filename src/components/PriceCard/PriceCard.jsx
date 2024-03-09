570d086789d27b7c8e20b3840681975f99998f9d
sadiaprova12 <sadiaprova51717@gmail.com>
2024-03-09 23:41:02 +0600

    added prices map

---
+++ src/components/PriceCard/PriceCard.jsx
@@ -0,0 +1,14 @@
+/* eslint-disable react/prop-types */
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+
+// eslint-disable-next-line react/prop-types
+const PriceCard = ({price}) => {
+    return (
+        <div>
+            {price.name}
+        </div>
+    );
+};
+
+export default PriceCard;
