20be10826f42e6f3891137d935252cb34054b999
sadiaprova12 <sadiaprova51717@gmail.com>
2024-03-09 22:42:29 +0600

    import price lsit

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@
 // import viteLogo from '/vite.svg'
 import './App.css'
 import Navbar from './components/Navbar/Navbar'
+import Price_List from './components/Price_List/Price_List'
 
 function App() {
   // const [count, setCount] = useState(0)
@@ -12,6 +13,7 @@ function App() {
      {/* Just practice */}
      <Navbar></Navbar>
      <h1 className='text-7xl text-purple-700'>Hello from Tailwind</h1>
+     <Price_List></Price_List>
 
     </div>
   )
