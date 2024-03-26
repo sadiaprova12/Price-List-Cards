894564078a740172411111448832fdb21fca4849
sadiaprova12 <sadiaprova51717@gmail.com>
2024-03-27 00:12:15 +0600

    change codes

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -6,64 +6,57 @@ import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/solid'
 
 
 const Navbar = () => {
+  const [open, setOpen] = useState(false);
 
-    const [open, setOpen] = useState(false);
+  const routes = [
+      {
+          id: 1,
+          name: 'Home',
+          path: '/home'
+      },
+      {
+          id: 2,
+          name: 'About',
+          path: '/about'
+      },
+      {
+          id: 3,
+          name: 'Contact',
+          path: '/contact'
+      },
+      {
+          id: 4,
+          name: 'Products',
+          path: '/products'
+      },
+      {
+          id: 5,
+          name: 'Services',
+          path: '/services'
+      }
+  ];
 
-    const routes = [
-        {
-          "id": 1,
-          "name": "Home",
-          "path": "/home"
-        },
-        {
-          "id": 2,
-          "name": "About",
-          "path": "/about"
-        },
-        {
-          "id": 3,
-          "name": "Services",
-          "path": "/services"
-        },
-        {
-          "id": 4,
-          "name": "Contact",
-          "path": "/contact"
-        },
-        {
-          "id": 5,
-          "name": "Products",
-          "path": "/products"
-        },
-      ];
-      
-    return (
-        <nav className='bg-purple-400'>
-            <div onClick={() => setOpen(!open)} className='md:hidden'>
-                <span>
-                    {
-                    open === true ? 
-                    <XMarkIcon className="h-6 w-6 text-purple-500"/>
-                    :<Bars3Icon className="h-6 w-6 text-purple-500"/>
-                
-                }
-                </span>
-                
-                
-            </div>
-              <ul className={`md:flex absolute md:static duration-500 pl-8 pb-4 py-2 bg-purple-400 ${open ? 'top-6': '-top-36'}`}>
-                {
-                    // eslint-disable-next-line react/jsx-key
-                    // routes.map(route => <li>{route.name}</li>)
-                    routes.map(route => <Link 
-                        key={route.id}
-                        route={route}
-                    ></Link>)
-                }
-              </ul>
-              {/* <BeakerIcon className="h-6 w-6 text-purple-500" /> */}
-        </nav>
-    );
+  return (
+      <nav className='bg-purple-400 px-4'>
+          <div onClick={() => setOpen(!open)} className='md:hidden'>
+              <span>
+                  {
+                      open === true ?
+                          <XMarkIcon className="h-6 w-6 text-purple-500" />
+                          : <Bars3Icon className="h-6 w-6 text-purple-500" />
+                  }
+              </span>
+          </div>
+          <ul className={`md:flex absolute md:static duration-500 pl-8 py-2 bg-purple-400 ${open ? 'top-6' : '-top-36'}`}>
+              {
+                  routes.map(route => <Link
+                      key={route.id}
+                      route={route}
+                  ></Link>)
+              }
+          </ul>
+      </nav>
+  );
 };
 
 export default Navbar;
